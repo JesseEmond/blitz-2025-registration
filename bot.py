97cfc54efb26be3d02c6c13737d053146a43aab5
JesseEmond <emond.jesse@gmail.com>
2024-09-30 18:48:46 -0400

    Only move in directions we can go

--- bot.py
+++ bot.py
@@ -2,6 +2,12 @@ from game_message import *
 import random
 
 
+# Seen in games. Crash if we've never seen a style so that we know.
+# TODO: Stop crashing on new types, for safety
+KNOWN_PERSONALITIES = ["lazy"]
+KNOWN_STYLES = ["bull", "hawk", "shark", "goldfish", "deer", "hawk", "owl"]
+
+
 class Bot:
     def __init__(self):
         print("Initializing your super mega duper bot")
@@ -10,18 +16,17 @@ class Bot:
         """
         Here is where the magic happens, for now the moves are not very good. I bet you can do better ;)
         """
-        actions = []
+        for threat in game_message.threats:
+            if threat.personality not in KNOWN_PERSONALITIES:
+                raise NotImplementedError(threat.personality)
+            if threat.style not in KNOWN_STYLES:
+                raise NotImplementedError(threat.style)
 
-        actions.append(
-            random.choice(
-                [
-                    MoveUpAction(),
-                    MoveRightAction(),
-                    MoveDownAction(),
-                    MoveLeftAction(),
-                ]
-            )
-        )
+        options = [d for d in Direction if game_message.can_go(d)]
+        actions = []
+        if options:
+            direction = random.choice(options)
+            actions.append(direction_to_action(direction))
 
         # You can clearly do better than the random actions above! Have fun!
         return actions
--- game_message.py
+++ game_message.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass, field
 from dataclasses_json import dataclass_json
-from enum import Enum, unique
+from enum import Enum, unique, auto
 from typing import Optional
+
+
+class Direction(Enum):
+    UP = auto()
+    DOWN = auto()
+    RIGHT = auto()
+    LEFT = auto()
 
 
 @unique
@@ -18,17 +25,35 @@ class Constants:
 
 @dataclass_json
 @dataclass
-class GameMap:
-    width: int
-    height: int
-    tiles: list[list[TileType]]
+class Position:
+    x: int
+    y: int
+
+    def offset(self, direction: Direction) -> "Position":
+        x, y = self.x, self.y
+        if direction == Direction.UP:
+            y -= 1
+        elif direction == Direction.DOWN:
+            y += 1
+        elif direction == Direction.LEFT:
+            x -= 1
+        elif direction == Direction.RIGHT:
+            x += 1
+        return Position(x, y)
 
 
 @dataclass_json
 @dataclass
-class Position:
-    x: int
-    y: int
+class GameMap:
+    width: int
+    height: int
+    # Note: tiles[x][y]
+    tiles: list[list[TileType]]
+
+    def is_empty(self, position: Position) -> bool:
+        if 0 <= position.x < self.width and 0 <= position.y < self.height:
+            return self.tiles[position.x][position.y] == TileType.EMPTY
+        return False
 
 
 @dataclass_json
@@ -63,6 +88,9 @@ class TeamGameState:
     threats: list[Threat]
     map: GameMap
 
+    def can_go(self, direction: Direction) -> bool:
+        return self.map.is_empty(self.yourCharacter.position.offset(direction))
+
 
 class Action:
     type: str
@@ -90,6 +118,12 @@ class MoveUpAction(Action):
 @dataclass
 class MoveDownAction(Action):
     type: str = "MOVE_DOWN"
+
+
+def direction_to_action(direction: Direction) -> Action:
+    directions = [Direction.UP, Direction.DOWN, Direction.LEFT, Direction.RIGHT]
+    actions = [MoveUpAction, MoveDownAction, MoveLeftAction, MoveRightAction]
+    return actions[directions.index(direction)]()
 
 
 @dataclass_json
