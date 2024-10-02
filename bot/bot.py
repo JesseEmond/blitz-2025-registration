ae720d01c851bea155c577e6159b118bc875cc75
JesseEmond <emond.jesse@gmail.com>
2024-10-01 23:18:09 -0400

    Add rust game state types, convert from python

--- bot/bot.py
+++ bot/bot.py
@@ -10,6 +10,29 @@ import devnull_bot
 # KNOWN_PERSONALITIES = ["lazy", "tease"]
 # KNOWN_STYLES = ["bull", "hawk", "shark", "goldfish", "deer", "hawk", "owl"]
 
+def create_rust_position(position: Position) -> devnull_bot.GamePosition:
+    return devnull_bot.GamePosition(x=position.x, y=position.y)
+
+
+def create_rust_threat(threat: Threat) -> devnull_bot.GameThreat:
+    return devnull_bot.GameThreat(
+        position=create_rust_position(threat.position),
+        style=threat.style)
+
+
+def create_rust_map(game_map: GameMap) -> devnull_bot.GameMap:
+    return devnull_bot.GameMap(
+        width=game_map.width,
+        height=game_map.height,
+        tiles=[[tile == TileType.WALL for tile in tiles_x] for tiles_x in game_map.tiles])
+
+
+def create_rust_game_state(game: TeamGameState) -> devnull_bot.GameState:
+    return devnull_bot.GameState(
+        tick=game.tick,
+        position=create_rust_position(game.yourCharacter.position),
+        threats=[create_rust_threat(t) for t in game.threats],
+        map=create_rust_map(game.map))
 
 class Bot:
     def __init__(self):
@@ -18,6 +41,7 @@ class Bot:
         self.prev_threats = None
 
     def get_next_move(self, game_message: TeamGameState):
+        create_rust_game_state(game_message)
         threats = [t.position for t in game_message.threats]
         if self.prev_threats is None:
             self.prev_threats = threats
--- bot/src/lib.rs
+++ bot/src/lib.rs
@@ -1,5 +1,80 @@
 use pyo3::prelude::*;
 
+#[pyclass]
+#[derive(Clone)]
+pub struct GamePosition {
+    #[pyo3(get, set)]
+    pub x: i32,
+    #[pyo3(get, set)]
+    pub y: i32,
+}
+
+#[pymethods]
+impl GamePosition {
+    #[new]
+    pub fn new(x: i32, y: i32) -> GamePosition {
+        GamePosition { x, y }
+    }
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct GameThreat {
+    #[pyo3(get, set)]
+    pub position: GamePosition,
+    #[pyo3(get, set)]
+    pub style: String,
+}
+
+#[pymethods]
+impl GameThreat {
+    #[new]
+    pub fn new(position: GamePosition, style: String) -> GameThreat {
+        GameThreat { position, style }
+    }
+}
+
+#[pyclass]
+#[derive(Clone)]
+pub struct GameMap {
+    #[pyo3(get, set)]
+    pub width: u32,
+    #[pyo3(get, set)]
+    pub height: u32,
+    /// Dims: [x][y], true if wall, false if empty.
+    #[pyo3(get, set)]
+    pub tiles: Vec<Vec<bool>>,
+}
+
+#[pymethods]
+impl GameMap {
+    #[new]
+    pub fn new(width: u32, height: u32, tiles: Vec<Vec<bool>>) -> GameMap {
+        GameMap { width, height, tiles }
+    }
+}
+
+#[pyclass]
+pub struct GameState {
+    #[pyo3(get, set)]
+    pub tick: u32,
+    #[pyo3(get, set)]
+    pub position: GamePosition,
+    #[pyo3(get, set)]
+    pub threats: Vec<GameThreat>,
+    #[pyo3(get, set)]
+    pub map: GameMap,
+}
+
+#[pymethods]
+impl GameState {
+    #[new]
+    pub fn new(tick: u32, position: GamePosition, threats: Vec<GameThreat>,
+               map: GameMap) -> GameState {
+        GameState { tick, position, threats, map }
+    }
+}
+
 #[pyfunction]
 fn hello_world() -> PyResult<u128> {
     println!("Hello world, from Rust!");
@@ -9,5 +84,9 @@ fn hello_world() -> PyResult<u128> {
 #[pymodule]
 fn devnull_bot(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(hello_world, m)?)?;
+    m.add_class::<GamePosition>()?;
+    m.add_class::<GameThreat>()?;
+    m.add_class::<GameMap>()?;
+    m.add_class::<GameState>()?;
     Ok(())
 }
