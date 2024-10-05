198f09f7bab389185de99847f99196439c88aa3b
JesseEmond <emond.jesse@gmail.com>
2024-10-05 19:21:56 -0400

    Replay ticks in a streaming fashion
    So we crash early, instead of reading the whole file first before
    starting to simulate.

--- bot/replayer.py
+++ bot/replayer.py
@@ -20,30 +20,21 @@ def parse_action(action: dict) -> Action:
     return next(a for a in actions if a.type == action["type"])
 
 
-def load_ticks(jsonl_path: str) -> list[Tick]:
-    with open(jsonl_path, "r") as f:
-        lines = [line.strip() for line in f.readlines()]
-    ticks = []
-    for line in lines:
-        stored = json.loads(line)
-        state = TeamGameState.from_dict(stored["game_message"])
-        actions = [parse_action(action) for action in stored["actions"]]
-        assert len(actions) <= 1, actions
-        ticks.append(Tick(state=state, action=actions[0] if actions else None))
-    return ticks
+def parse_tick(json_line: str) -> Tick:
+    stored = json.loads(json_line)
+    state = TeamGameState.from_dict(stored["game_message"])
+    actions = [parse_action(action) for action in stored["actions"]]
+    assert len(actions) <= 1, actions
+    return Tick(state=state, action=actions[0] if actions else None)
 
 
-def replay(ticks: list[Tick]) -> None:
-    assert len(ticks) > 0
-    start_state = rust_interop.to_rust_game_state(ticks[0].state)
-    bot = devnull_bot.DevnullBot(start_state)
-    for tick in ticks:
-        state = rust_interop.to_rust_game_state(tick.state)
-        action = tick.action
-        if isinstance(action, MoveToAction):
-            bot.simulate_move_to(state, rust_interop.to_rust_position(action.position))
-        else:
-            bot.simulate(state, rust_interop.to_rust_action(action))
+def replay(bot: devnull_bot.DevnullBot, tick: Tick) -> None:
+    state = rust_interop.to_rust_game_state(tick.state)
+    action = tick.action
+    if isinstance(action, MoveToAction):
+        bot.simulate_move_to(state, rust_interop.to_rust_position(action.position))
+    else:
+        bot.simulate(state, rust_interop.to_rust_action(action))
 
 
 if __name__ == "__main__":
@@ -51,7 +42,11 @@ if __name__ == "__main__":
         print("Usage: replayer.py games/1.jsonl")
         exit(1)
     jsonl_path = sys.argv[1]
-    print("Loading ticks...")
-    ticks = load_ticks(jsonl_path)
-    print("Replaying ticks...")
-    replay(ticks)
+    bot = None
+    with open(jsonl_path, "r") as f:
+        for line in f.readlines():
+            tick = parse_tick(line)
+            if bot is None:
+                start_state = rust_interop.to_rust_game_state(tick.state)
+                bot = devnull_bot.DevnullBot(start_state)
+            replay(bot, ticks)
