eff0d1d577efdc62052ee7b3588ed305f130c310
JesseEmond <emond.jesse@gmail.com>
2024-10-07 00:23:05 -0400

    Replace minimax with mcts lib search
    Confirmed can still get 10k on that same map we previously got it on.

--- bot/src/search.rs
+++ bot/src/search.rs
@@ -1,101 +1,35 @@
-use rand::seq::SliceRandom;
 use crate::grid::{Move, Pos};
 use crate::mcts;
 use crate::simulation::{Game, SimulationAction, State};
 
-type Score = i32;
-
-trait Evaluator {
-    /// Higher is better for the player.
-    fn evaluate(&self, state: &State) -> Score;
-}
-
 /// Example eval where our both should move right when it can.
 struct MoveRightEval;
-impl Evaluator for MoveRightEval {
-    fn evaluate(&self, state: &State) -> Score {
-        state.pos.x as Score
+impl mcts::Evaluator<MCTS> for MoveRightEval {
+    fn evaluate(&self, state: &State) -> mcts::Score {
+        state.pos.x as mcts::Score
     }
 }
 struct MoveRightAliveEval;
-impl Evaluator for MoveRightAliveEval {
-    fn evaluate(&self, state: &State) -> Score {
+impl mcts::Evaluator<MCTS> for MoveRightAliveEval {
+    fn evaluate(&self, state: &State) -> mcts::Score {
         if !state.game_over {
-            state.pos.x as Score
+            state.pos.x as mcts::Score
         } else {
-            -50000 + state.tick as Score
+            -50000 as mcts::Score + state.tick as mcts::Score
         }
     }
 }
 
 struct ThreatsAreFarEval;
-impl Evaluator for ThreatsAreFarEval {
-    fn evaluate(&self, state: &State) -> Score {
+impl mcts::Evaluator<MCTS> for ThreatsAreFarEval {
+    fn evaluate(&self, state: &State) -> mcts::Score {
         if !state.game_over {
             // TODO: Worth doing a proper pathfinding distance?
             state.threats.iter().map(|t| t.pos.manhattan_dist(&state.pos))
-                .min().unwrap() as Score
+                .min().unwrap() as mcts::Score
         } else {
-            -50000 + state.tick as Score
+            -50000 as mcts::Score + state.tick as mcts::Score
         }
-    }
-}
-
-trait Search {
-    fn choose_move(&self, state: &State, evaluator: &dyn Evaluator) -> Option<Move>;
-}
-
-/// Example search implementation that just picks the first available move.
-struct FirstMoveSearch;
-impl Search for FirstMoveSearch {
-    fn choose_move(&self, state: &State, _evaluator: &dyn Evaluator) -> Option<Move> {
-        return state.generate_moves()[0]
-    }
-}
-
-// TODO: Minimax is not needed now that it's a single player game again
-struct MinimaxSearch {
-    max_depth: i8,
-}
-impl MinimaxSearch {
-    fn search(&self, state: &State, evaluator: &dyn Evaluator, depth: i8) -> Score {
-        if depth == 0 || state.game_over {
-            return evaluator.evaluate(state);
-        }
-        let scores = state.generate_moves().into_iter()
-                .map(|m| self.move_value(state, m, evaluator, depth));
-        scores.max().unwrap()  // always maximize, no enemy turns
-    }
-
-    fn move_value(&self, state: &State, action: Option<Move>,
-                  evaluator: &dyn Evaluator, depth: i8) -> Score {
-        let mut state = state.clone();
-        state.simulate_tick(SimulationAction::Move { direction: action });
-        self.search(&state, evaluator, depth - 1)
-    }
-
-    fn get_max_score_moves<F>(&self, moves: Vec<Option<Move>>, score_fn: F) -> Vec<Option<Move>> where 
-        F: Fn(Option<Move>) -> Score {
-        let move_scores: Vec<Score> = moves.iter().cloned().map(score_fn).collect();
-        let max_score = *move_scores.iter().max().unwrap();
-        moves.iter().zip(move_scores.iter())
-            .filter(|(_, &s)| s == max_score)
-            .map(|(&m, _)| m)
-            .collect()
-    }
-}
-impl Search for MinimaxSearch {
-    fn choose_move(&self, state: &State, evaluator: &dyn Evaluator) -> Option<Move> {
-        // If we just pick the one with the highest score, we might end up with
-        // "lazy" moves, where both a move and None could get to a position at
-        // depth D. To make this less common, of the best final moves, greedily
-        // pick the next moves. For equal greediness, randomly pick.
-        let moves = state.generate_moves();
-        let best_moves = self.get_max_score_moves(
-            moves, |m| self.move_value(state, m, evaluator, self.max_depth));
-        let greedy_moves = self.get_max_score_moves(
-            best_moves, |m| self.move_value(state, m, evaluator, 1));
-        *greedy_moves.choose(&mut rand::thread_rng()).unwrap()
     }
 }
 
@@ -108,13 +42,6 @@ impl mcts::MCTS for MCTS {
     type Evaluator = ThreatsAreFarEval;
     type Budget = mcts::TimeBudget;
     type RolloutPolicy = mcts::RandomPolicy;
-}
-
-// TODO: Remove once we can get rid of non-mcts 'Evaluator'
-impl<E> mcts::Evaluator<MCTS> for E where E: Evaluator {
-    fn evaluate(&self, state: &State) -> mcts::Score {
-        self.evaluate(state) as mcts::Score
-    }
 }
 
 impl mcts::SearchState<MCTS> for State {
@@ -134,19 +61,16 @@ impl Bot {
     /// Update state based on 'game', pick our next move, apply it locally.
     pub fn pick_move(&mut self, game: &Game) -> Option<Move> {
         self.state.verify_predictions(game);
-        let strategy = MinimaxSearch { max_depth: 7 };
-        let picked = strategy.choose_move(&self.state, &ThreatsAreFarEval{});
-
         // TODO: Algorithm definition should be within the mcts namespace
         // From https://arxiv.org/pdf/1208.4692 , this is like iterative
         // sampling.
-        // let simulate = mcts::Simulate::<MCTS>::new(mcts::RandomPolicy {});
-        // let params = mcts::SearchParams::<MCTS>::new(
-        //     mcts::TimeBudget { max_time: std::time::Duration::from_millis(75) },
-        //     ThreatsAreFarEval {});
-        // let algorithm = mcts::Algorithm::<MCTS>::new(Box::new(simulate), params);
-        // let results = algorithm.search(&self.state);
-        // let picked = results.next_action().expect("search empty results");
+        let simulate = mcts::Simulate::<MCTS>::new(mcts::RandomPolicy {});
+        let params = mcts::SearchParams::<MCTS>::new(
+            mcts::TimeBudget { max_time: std::time::Duration::from_millis(75) },
+            ThreatsAreFarEval {});
+        let algorithm = mcts::Algorithm::<MCTS>::new(Box::new(simulate), params);
+        let results = algorithm.search(&self.state);
+        let picked = results.next_action().expect("search empty results");
 
         self.state.simulate_tick(SimulationAction::Move { direction: picked });
         picked
